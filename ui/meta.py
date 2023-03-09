31c4fea1bbe81ad884ffe2fa196dce7e570cba97
Tarn Yeong Ching <i@ctarn.io>
2023-03-09 22:04:07 +0800

    increase version to 1.0.2

--- ui/meta.py
+++ ui/meta.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 name = "PepPre"
-version = "1.0.1"
+version = "1.0.2"
 author = "Tarn Yeong Ching"
 url = f"http://{name.lower()}.ctarn.io"
 server = f"http://api.ctarn.io/{name}/{version}"
