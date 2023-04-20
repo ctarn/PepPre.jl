83aa4b460f5591f6c04dfae3891a278bccfaeb69
Tarn Yeong Ching <i@ctarn.io>
2023-04-20 21:24:15 +0800

    ui: adjust `homedir`

--- ui/meta.py
+++ ui/meta.py
@@ -7,4 +7,4 @@ author = "Tarn Yeong Ching"
 url = f"http://{name.lower()}.ctarn.io"
 server = f"http://api.ctarn.io/{name}/{version}"
 copyright = f"{name} {version}\nCopyright © 2023 {author}\n{url}"
-homedir = os.path.join(Path.home(), f".{name}", version)
+homedir = os.path.join(Path.home(), f".{name}", f"v{'.'.join(version.split('.')[0:2])}")
